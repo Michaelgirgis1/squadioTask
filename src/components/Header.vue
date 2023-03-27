63616e19174f141c2b80af49aeaca26b625a4467
Michael Grerges <mgerges@TradeNET.COM>
2023-03-27 10:13:04 +0200

    add footer add header make home commponent

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header text-center">
+    <!-- <h1>Sure Task</h1> -->
+    <h1>Squadio Task</h1>
+
+  </header>
+</template>
+
+<script>
+export default {
+    name: "headerApp",
+}
+</script>
+
+<style lang="scss">
+.header {
+  background-color: rgba(230,230,230,.9);
+  padding: 5px 0;
+  h1 {
+    color: #000;
+    font-size: 35px;
+    font-weight: bold;
+  }
+}
+</style>
